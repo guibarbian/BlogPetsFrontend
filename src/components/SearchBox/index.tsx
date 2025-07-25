760d56b9248926bb27f307eb89eff93c183d9a3f
Guilherme Barbian <guiabarbian2@gmail.com>
2025-07-25 13:21:59 -0300

    refactor: improved html logic

--- src/components/SearchBox/index.tsx
+++ src/components/SearchBox/index.tsx
@@ -1,8 +1,8 @@
-import styles from './styles.module.css'
+import './styles.module.css'
 
 const SearchBox = ( {...props}:  React.InputHTMLAttributes<HTMLInputElement>) => {
   return (
-    <input className={styles.input} {...props} />
+    <input {...props} />
   )
 }
 
